af67141920a1af80711285378a85ac2d31231ff0
sartaj_work <sartajahamad42889@gmail.com>
2025-09-08 21:50:48 +0530

    images some isseu solve not 100 percent

--- sartaj_portfolio/settings.py
+++ sartaj_portfolio/settings.py
@@ -42,6 +42,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',    # ADD THIS LINE
+                'django.template.context_processors.media',     # ADD THIS LINE
             ],
         },
     },
